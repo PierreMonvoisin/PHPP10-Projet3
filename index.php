947437529d91386ec7e8475b96f27b654e88c144
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2020-01-02 21:20:48 +0100

    TP terminé

--- index.php
+++ index.php
@@ -5,13 +5,12 @@ $portrait2 = array('name'=>'Jean', 'firstname'=>'de La Fontaine', 'portrait'=>'h
 $portrait3 = array('name'=>'Pierre', 'firstname'=>'Corneille', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');
 $portrait4 = array('name'=>'Jean', 'firstname'=>'Racine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');
 // Texte associé aux personnes des tableaux
-$victorBio = 'Victor Hugo, né le 26 février 1802 à Besançon et mort le 22 mai 1885 à Paris, est un poète lyrique, engagé ou épique selon les recueils ; romancier du peuple qui rencontre un grand succès avec Notre-Dame de Paris (1831), et plus encore avec Les Misérables (1862).';
+$victorBio = 'Victor Hugo, né le 26 février 1802 à Besançon et mort le 22 mai 1885 à Paris, est un poète lyrique ou engagé; romancier du peuple qui rencontre un grand succès avec Notre-Dame de Paris (1831) ainsi que Les Misérables (1862).';
 $jeanLfBio = 'Jean de La Fontaine est un poète français né à Château-Thierry en 1621 et mort à Paris en 1695. Il est surtout connu pour être l\'auteur de ses très célèbres Fables.';
 $pierreBio = 'Corneille est né à Rouen le 6 juin 1606 dans une famille bourgeoise. Il fait de brillantes études au collège des Jésuites de Rouen (de 1615 à 1622) et devient avocat en 1624.';
 $jeanRBio = 'Célèbre poète, auteur de nombreuses tragédies durant la période classique, Jean Racine est connu dans le monde de l\'écriture grâce au succès de la pièce d\'Andromaque en 1667. Il décède à Paris le 21 avril 1699, à l\'âge de 59 ans.';
 // Déclaration d'un tableau de tableaux
-$portraits = [$portrait1, $portrait2, $portrait3, $portrait4];
-?>
+$portraits = [$portrait1, $portrait2, $portrait3, $portrait4]; ?>
 <!DOCTYPE html>
 <html lang='fr' dir='ltr'>
 <head>
@@ -21,29 +20,40 @@ $portraits = [$portrait1, $portrait2, $portrait3, $portrait4];
   <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
   <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 </head>
-<body class='container-fluid'>
-  <?php foreach($portraits as $array){
+<body class='container-fluid d-flex flex-wrap pt-5'>
+  <?php
+  // Pour chaque array du array principale
+  foreach($portraits as $array){
+    // Pour chaque clef associée à une valeur
     foreach($array as $key => $value){
-      echo 'proot';
-      // Still need to echo all value from all arrays in array
+      // Récupération des informations pour création d'une carte de présentation
+      if($key == 'portrait'){ $imageURL = $value; }
+      elseif ($key == 'firstname') { $lastName = $value; }
+      else { $firstName = $value; }
     }
-  } ?>
-  <div class="card mx-auto mb-3 w-50">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img src="http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg" class="card-img" alt="Victor Hugo">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">Victor Hugo</h5>
-          <p class="card-text"></p>
+    // Association de la personne avec sa description
+    if (strlen($lastName) < 5){ $description = $victorBio; }
+    elseif (strlen($lastName) < 7){ $description = $jeanRBio; }
+    elseif (strlen($lastName) < 10){ $description = $pierreBio; }
+    else {$description = $jeanLfBio; }
+    ?>
+    <!-- Création d'une carte pour chaque sous-tableau -->
+    <div class="card col-md-6 col-sm-12 mx-auto mb-3">
+      <div class="row no-gutters">
+        <div class="col-md-4 d-flex">
+          <img src="<?= $imageURL ?>" class="card-img m-auto" alt="<?= $firstName.$lastName ?>">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title text-center mb-4"><?= $firstName. ' ' .$lastName ?></h5>
+            <p class="card-text text-justify"><?= $description ?></p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  <?php } ?>
   <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
   <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
-  <script src='assets/js/script.js'></script>
 </body>
 </html>
